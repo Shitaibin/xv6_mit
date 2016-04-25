21e7ff66667347ab51d3ef8092d6bae98c2bc894
shitaibin <hz_stb@163.com>
2016-04-25 06:42:27 -0700

    lab1, finish backtrace

--- kern/monitor.c
+++ kern/monitor.c
@@ -63,6 +63,11 @@ mon_backtrace(int argc, char **argv, struct Trapframe *tf)
   // cprintf("Wow, this is the backtrace routine.\n");
   uint32_t *ebp, *eip;
   uint32_t arg0, arg1, arg2, arg3, arg4;
+  struct Eipdebuginfo eip_debug_info_obj;
+  struct Eipdebuginfo *info;
+  char eip_fn_name[30];
+  
+  info = &eip_debug_info_obj;
 
   ebp = (uint32_t *)read_ebp();
   eip = (uint32_t *)ebp[1];   /* in backtrace eip point to where return */
@@ -71,11 +76,16 @@ mon_backtrace(int argc, char **argv, struct Trapframe *tf)
   arg2 = ebp[4];
   arg3 = ebp[5];
   arg4 = ebp[6];
-  
+  debuginfo_eip((uintptr_t)eip, info);
+  memcpy(eip_fn_name, info->eip_fn_name, info->eip_fn_namelen);
+  eip_fn_name[info->eip_fn_namelen] = 0;
+
   cprintf("Stack backtrace:\n");
   while (ebp != 0) {  /* the .... */
     cprintf("  ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n",
         ebp, eip, arg0, arg1, arg2, arg3, arg4);
+    cprintf("         %s:%d: %s+%d\n", info->eip_file, info->eip_line,
+        eip_fn_name, (int)((uintptr_t)eip - info->eip_fn_addr));
     ebp = (uint32_t *)ebp[0];
     eip = (uint32_t *)ebp[1];   /* in backtrace eip point to where return */
     arg0 = ebp[2];
@@ -83,6 +93,10 @@ mon_backtrace(int argc, char **argv, struct Trapframe *tf)
     arg2 = ebp[4];
     arg3 = ebp[5];
     arg4 = ebp[6];
+    
+    debuginfo_eip((uintptr_t)eip, info);
+    memcpy(eip_fn_name, info->eip_fn_name, info->eip_fn_namelen);
+    eip_fn_name[info->eip_fn_namelen] = 0;
   }
 	return 0;
 }
