824352fe8220814bec9796303de02333c5a69e7f
shitaibin <hz_stb@163.com>
2016-05-13 19:38:11 +0800

    lab4, exercise 3

--- kern/pmap.c
+++ kern/pmap.c
@@ -284,7 +284,14 @@ mem_init_mp(void)
 	//     Permissions: kernel RW, user NONE
 	//
 	// LAB 4: Your code here:
-
+  size_t i;
+  for (i = 0; i < NCPU; i++) {
+    boot_map_region(kern_pgdir,
+                    KSTACKTOP - KSTKSIZE - i * (KSTKSIZE + KSTKGAP),
+                    KSTKSIZE,
+                    PADDR(percpu_kstacks[i]),
+                    PTE_W);
+  }
 }
 
 // --------------------------------------------------------------
